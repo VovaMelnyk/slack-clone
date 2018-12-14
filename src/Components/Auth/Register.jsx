fcf9f39ce6c2b487e680c52a370ca5c35fe7aba6
Vova Melnyk <hagmen4339@gmail.com>
2018-12-14 23:49:56 +0200

    add form validation

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,111 +1,153 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import firebase from '../../firebase';
 import {NavLink} from 'react-router-dom'
 import {
-    Grid,
-    Form,
-    Segment,
-    Button,
-    Header,
-    Message,
-    Icon
-  } from 'semantic-ui-react';
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from 'semantic-ui-react';
 class Register extends Component {
 
-    state = {
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirm: '',
-      }
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    errors: [],
+  }
 
-      handlerChange = (e) => {
-        this.setState({
-          [e.target.name]: e.target.value
-        })
-      }
+  handlerChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
-      handleSubmit = (e) => {
-        e.preventDefault();
-        firebase
+  isFormEmpty = ({username, email, password, passwordConfirm}) => {
+    return !username.length || !email.length || !password.length || !passwordConfirm.length;
+  }
+
+  isPasswordValid = ({password, passwordConfirm}) => {
+    return password === passwordConfirm;
+  }
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+    if (this.isFormEmpty(this.state)) {
+      error = {
+        message: 'Fill in all fields'
+      };
+      this.setState({
+        errors: errors.concat(error)
+      })
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = {
+        message: 'Password is invalid'
+      };
+      this.setState({
+        errors: errors.concat(error)
+      })
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.isFormValid()) {
+      firebase
         .auth()
         .createUserWithEmailAndPassword(this.state.email, this.state.password)
         .then(createdUser => {
-            console.log(createdUser);
+          console.log(createdUser);
         })
         .catch(err => {
-            console.error(err);
+          console.error(err);
         })
-      }
-
-
-    render() {
-        const {username, email, password, passwordConfirm} = this.state
-
-        return (
-            <Grid textAlign='center' verticalAlign='middle' className='app'>
-            <Grid.Column style={{
-              maxWidth: 450
-            }}>
-              <Header as='h2' icon color='orange' textAlign='center'>
-                <Icon name='comment alternate' color='orange'/>
-                Register for Slack Clone
-              </Header>
-              <Form size='large' onSubmit={this.handleSubmit}>
-                <Segment stacked>
-                  <Form.Input
-                    fluid
-                    name='username'
-                    icon='user'
-                    iconPosition='left'
-                    placeholder='Username'
-                    type='text'
-                    onChange={this.handlerChange}
-                    value={username}/>
-    
-                  <Form.Input
-                    fluid
-                    name='email'
-                    icon='mail'
-                    iconPosition='left'
-                    placeholder='Email'
-                    type='email'
-                    onChange={this.handlerChange}
-                    value={email}/>
-    
-                  <Form.Input
-                    fluid
-                    name='password'
-                    icon='lock'
-                    iconPosition='left'
-                    placeholder='Password'
-                    type='password'
-                    onChange={this.handlerChange}
-                    value={password}/>
-    
-                  <Form.Input
-                    fluid
-                    name='passwordConfirm'
-                    icon='repeat'
-                    iconPosition='left'
-                    placeholder='Password Confirm'
-                    type='password'
-                    onChange={this.handlerChange}
-                    value={passwordConfirm}/>
-    
-                  <Button color='orange' fluid size='large'>
-                    Submit
-                  </Button>
-                </Segment>
-              </Form>
-              <Message>
-                Already a user?
-                <NavLink to='/login'>Login</NavLink>
-              </Message>
-            </Grid.Column>
-          </Grid>
-        );
     }
+  }
+
+
+
+  render() {
+    const {username, email, password, passwordConfirm, errors} = this.state
+
+    return (
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
+        <Grid.Column style={{
+          maxWidth: 450
+        }}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='comment alternate' color='orange'/>
+            Register for Slack Clone
+          </Header>
+          <Form size='large' onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name='username'
+                icon='user'
+                iconPosition='left'
+                placeholder='Username'
+                type='text'
+                onChange={this.handlerChange}
+                value={username}/>
+
+              <Form.Input
+                fluid
+                name='email'
+                icon='mail'
+                iconPosition='left'
+                placeholder='Email'
+                type='email'
+                onChange={this.handlerChange}
+                value={email}/>
+
+              <Form.Input
+                fluid
+                name='password'
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
+                onChange={this.handlerChange}
+                value={password}/>
+
+              <Form.Input
+                fluid
+                name='passwordConfirm'
+                icon='repeat'
+                iconPosition='left'
+                placeholder='Password Confirm'
+                type='password'
+                onChange={this.handlerChange}
+                value={passwordConfirm}/>
+
+              <Button color='orange' fluid size='large'>
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {errors.map(el => <p key={el.message}>{el.message}</p>)}
+            </Message>
+          )}
+          <Message>
+            Already a user?
+            <NavLink to='/login'>Login</NavLink>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default Register;
