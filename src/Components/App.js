0e597b1837d169611090adf56c02a77974f84201
Vova Melnyk <hagmen4339@gmail.com>
2018-12-15 01:15:49 +0200

    app markup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Grid} from 'semantic-ui-react';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 import './App.css';
+const App = () => {
+  return (
+    <Grid columns='equal' className='app'>
+      <ColorPanel/>
+      <SidePanel/>
+      <Grid.Column textAlign='center'>
+        <Messages/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel/>
+      </Grid.Column>
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      App
-      </div>
-    );
-  }
-}
+    </Grid>
+  );
+};
 
 export default App;
