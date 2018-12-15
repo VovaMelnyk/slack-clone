53863997744a39a7f7ef7abaa52c30389a971fdd
Vova Melnyk <hagmen4339@gmail.com>
2018-12-15 23:54:06 +0200

    removeListeners

--- src/Components/Channels/Channels.jsx
+++ src/Components/Channels/Channels.jsx
@@ -20,6 +20,14 @@ class Channels extends Component {
         this.addListeners();
     }
 
+    componentWillUnmount () {
+        this.removeListeners()
+    }
+
+    removeListeners = () => {
+        this.state.channelsRef.off();
+    }
+
     addListeners = () => {
         let loadedChanels = [];
         this.state.channelsRef.on('child_added',snap => {
