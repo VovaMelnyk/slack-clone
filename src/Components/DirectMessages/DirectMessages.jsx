f0706b3c594e6896a0099ef4fba8080fdb66b951
Vova Melnyk <hagmen4339@gmail.com>
2018-12-22 19:29:38 +0200

    display direct user

--- src/Components/DirectMessages/DirectMessages.jsx
+++ src/Components/DirectMessages/DirectMessages.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import firebase from '../../firebase';
 
 class DirectMessages extends React.Component {
   state = {
-    users: []
+    users: [],
+    usersRef: firebase.database().ref('users')
+  }
+
+  componentDidMount() {
+    if(this.props.user) {
+      this.addListener(this.props.user.uid)
+    }
+  }
+
+  addListener = id => {
+    let loadedUsers = [];
+    this.state.usersRef.on('child_added',snap => {
+      if(id !== snap.key) {
+        let user = snap.val();
+        user.uid = snap.key;
+        user.status = 'offline';
+        loadedUsers.push(user);
+        this.setState({
+          users: loadedUsers
+        })
+      }
+    })
   }
 
   render() {
@@ -17,9 +41,21 @@ class DirectMessages extends React.Component {
           </span> ({ users.length })
         </Menu.Item>
         {/* Users to Send Direct Messages */}
+          {users.map(el => <Menu.Item
+          key={el.uid}
+          onClick={()=> console.log(el)}
+          style={{opacity:0.7, fontStyle: 'italic'}}
+          >
+          <Icon name='circle'/>
+          @ {el.name}
+          </Menu.Item>)}
       </Menu.Menu>
     )
   }
 }
 
-export default DirectMessages;
+const mapStateToProps = state => ({
+  user: state.user.currentUser,
+})
+
+export default connect(mapStateToProps)(DirectMessages);
